2bf8ae4117c926d8fb44366266333b4cff37d09e
OkaforF <fionaokafor@gmail.com>
2019-02-22 12:56:33 +0100

    deleted

--- mbank/chat-2/api-get-messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//who are you, whose messages should i get?
-
-$sUserId = $_GET['sUserId'];
-$sMessages = file_get_contents('to-'.$sUserId.'.txt');
-file_put_contents('to-'.$sUserId.'.txt','');
-
-echo $sMessages;
